883a51a611cc7e4195844c11bf24747a01ddbbea
github username <github email address>
2020-01-22 10:00:55 -0500

    added BOOTSTRAP

--- src/components/ListItemShow.js
+++ src/components/ListItemShow.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
 
 class ListItemShow extends React.Component {
     render() {
         return(
-            <div>
-                <h5>Title: {this.props.movie.title}</h5>
-                <h5>Year: {this.props.movie.year}</h5>
-                <img src={this.props.movie.imageUrl} alt={this.props.movie.title} />
-            </div>
+            <Card style={{ width: '36rem' }}>
+                <Card.Body>
+                    <Row>
+                        <Col xs lg="1">                
+                            <h1>{this.props.movie.rank}</h1>
+                        </Col>
+                        <Col lg="2">
+                            <Image src={this.props.movie.imageUrl} alt={this.props.movie.title} width="75" thumbnail/>
+                        </Col>
+                        <Col>
+                            <h2> {this.props.movie.title}</h2>
+                            <h5> {this.props.movie.year}</h5>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
         )
     }
 
 }
+
+
+            //             <Image src={this.props.movie.Poster} alt={this.props.movie.Title} thumbnail/>
+            //         </Col>
+            //         <Col>
+            //             <h5>Title: {this.props.movie.Title}</h5>
+            //             <h5>Year: {this.props.movie.Year}</h5>
+            //         </Col>
+            //     </Row>
+            // </Container>
 
 export default ListItemShow;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom' 
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 
 const NavBar = () => {
     return(
-        <div>
-            I am ze NavBar.
-            <br></br>
-            <Link to='/signup'>Sign Up</Link>
-            <br></br>
-            <Link to='/login'>Log In</Link>
-            <br></br>
-            <Link to='/home'>Home</Link>
-            <br></br>
-            <Link to='/profiles'>All Profiles</Link>
-            <br></br>
-            <Link to='/profiles/1'>My Profile</Link>
-            <br></br>
-            <Link to='/profiles/1/list/new'>Create New List</Link>
-            <br></br>
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href='/home'>Ze NavBar</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href='/profiles/1'>My Profile</Nav.Link>
+            <Nav.Link href='/profiles'>Profiles</Nav.Link>
+            </Nav>
+            <Button href='/profiles/1/list/new' variant="light">Create List</Button>
+        </Navbar>
+
     )
 }
 
 export default NavBar;
+
+        // <div>
+        //     I am ze NavBar.
+        //     <br></br>
+        //     <Link to='/signup'>Sign Up</Link>
+        //     <br></br>
+        //     <Link to='/login'>Log In</Link>
+        //     <br></br>
+        //     <Link to='/home'>Home</Link>
+        //     <br></br>
+        //     <Link to='/profiles'>All Profiles</Link>
+        //     <br></br>
+        //     <Link to='/profiles/1'>My Profile</Link>
+        //     <br></br>
+        //     <Link to='/profiles/1/list/new'>Create New List</Link>
+        //     <br></br>
+        // </div>
